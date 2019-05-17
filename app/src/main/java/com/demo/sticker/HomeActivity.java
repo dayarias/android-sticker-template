310594c9588b794480cbeebaa8fe032ea41f128b
Carol Arias <dayarias@gmail.com>
2019-05-17 17:52:44 -0500

    First Alpha Stickers Test

--- app/src/main/java/com/demo/sticker/HomeActivity.java
+++ app/src/main/java/com/demo/sticker/HomeActivity.java
@@ -111,17 +111,8 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public static Sticker[] stickers = {
-        new Sticker("logo black", R.drawable.pg_logo_black,
-                "https://firebasestorage.googleapis.com/v0/b/gboardcustomsticker-fea6d.appspot.com/o/Pg-Logo-Black.png?alt=media&token=e07c50df-697a-40d7-a6a1-a8f5f53f7bba", new String [] {"playground","logo","black"}),
-        new Sticker("logo blue", R.drawable.pg_logo_blue,
-                    "https://firebasestorage.googleapis.com/v0/b/gboardcustomsticker-fea6d.appspot.com/o/Pg-Logo-Blue.png?alt=media&token=c1400515-36e9-4f2e-9e78-5171bdfa5339", new String [] {"playground","logo","blue"}),
-        new Sticker("logo gold", R.drawable.pg_logo_gold,
-                    "https://firebasestorage.googleapis.com/v0/b/gboardcustomsticker-fea6d.appspot.com/o/Pg-Logo-Gold.png?alt=media&token=0154c57c-ec2f-4f32-b706-f81fd68788d4", new String [] {"playground","logo","gold"}),
-        new Sticker("logo green", R.drawable.pg_logo_green,
-                    "https://firebasestorage.googleapis.com/v0/b/gboardcustomsticker-fea6d.appspot.com/o/Pg-Logo-Green.png?alt=media&token=2d5b1d18-230f-4401-a27d-1e035ced180a", new String [] {"playground","logo","green"}),
-        new Sticker("logo light blue", R.drawable.pg_logo_lightblue,
-                    "https://firebasestorage.googleapis.com/v0/b/gboardcustomsticker-fea6d.appspot.com/o/Pg-Logo-LightBlue.png?alt=media&token=0fa1713e-f589-4b1b-8ec7-5b81de8e1c3d", new String [] {"playground","logo","light blue"}),
-        new Sticker("logo red", R.drawable.pg_logo_red,
-                    "https://firebasestorage.googleapis.com/v0/b/gboardcustomsticker-fea6d.appspot.com/o/Pg-Logo-Red.png?alt=media&token=be40cb08-09e3-4f4b-808c-e81a0693fcac", new String [] {"playground","logo","red"})
-    };
+            new Sticker("Duccky", R.drawable.duccky,
+                     "https://drive.google.com/file/d/1FLyCYIdG4gQpyubDMcJ_HrTRRSwhX-fL/view?usp=sharing",
+                    new String [] {"Pato1"})
+            };
 }
